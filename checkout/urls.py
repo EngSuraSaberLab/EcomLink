8b9e067f4e67ca06d96fad898ba6d60e00443ee2
Sura Saber <YOUR_GITHUB_EMAIL@example.com>
2026-03-31 01:12:24 +0300

    Remove files not needed for GitHub Pages

--- checkout/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.checkout, name='checkout'),
-    path('complete/', views.checkout_complete, name='checkout_complete'),
-]
